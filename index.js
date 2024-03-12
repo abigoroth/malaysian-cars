7ccf28271fd0c455942cd2caf47a338ecd446de4
yakob.abaidullah <yakob.abaidullah@ideagen.com>
2024-03-13 00:07:42 +0800

    useful right now

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 const data = require("./data.json");
+const allMakes = data.makes;
+const allModels = data.models;
+
+const getModels = makerName => {
+  let models = [];
+  const makeEl = allMakes.find((x) => x.name === makerName);
+  console.log("@@@@")
+  console.log(makeEl);
+  allModels.filter(modelFilter => {
+    if (modelFilter.parent_id === makeEl.id) {
+      const modelList = modelFilter.values;
+
+      modelList.map(modelFilter2 => {
+        models.push(modelFilter2.name);
+
+        return null;
+      });
+    }
+    return null;
+  });
+
+  return models;
+};
+
+module.exports.allMakes = allMakes;
+module.exports.allModels = allModels;
+
+module.exports.getModels = getModels;
+
